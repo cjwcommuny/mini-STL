b17c3c043fd6af3c233937972d46bd82b3e81990
cjwcommuny <cjwcommuny@outlook.com>
2018-06-15 13:16:33 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/mini_STL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_STL.dir/main.cpp.o"
+  "mini_STL.pdb"
+  "mini_STL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_STL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
